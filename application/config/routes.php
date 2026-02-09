04a8dcdbf01e9d3a281b8e5659e537b58c1eb018
mknabb92-blip <mknabb92@gmail.com>
2026-02-09 05:23:46 -0800

    feat(db): designing user db

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'students_controller';
+$route['default_controller'] = 'Migrate';
 $route['student'] = 'students_controller';
 $route['translate_uri_dashes'] = FALSE;
