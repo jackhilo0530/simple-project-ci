cca67c450d443805f35b966f412ed2b6476b1ece
jackhilo0530 <hilo0530@outlook.com>
2026-02-10 05:44:42 -0800

    Update routes.php
    I have fixed default_controller.

--- application/config/routes.php
+++ application/config/routes.php
@@ -49,6 +49,5 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'Migrate';
-$route['student'] = 'students_controller';
+$route['default_controller'] = 'pages/view';
 $route['translate_uri_dashes'] = FALSE;
